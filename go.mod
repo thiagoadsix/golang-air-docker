c3360b113f0dfb69bd69cb6ccd70316a0ab2646f
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-08-12 22:06:47 -0300

    chore: update go.mod and go.sum files with go-chi v1.5.5 dependency to run our server

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thiagoadsix/golang-docker-air
 
 go 1.22.5
+
+require github.com/go-chi/chi v1.5.5
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+
+	fmt.Println("Server starting at http://localhost:3000")
+
+	err := http.ListenAndServe(":3000", r)
+	if err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
 }
